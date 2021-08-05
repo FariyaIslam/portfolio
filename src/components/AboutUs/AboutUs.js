2b3b4ac6a751b3d34dfc5ad1d4e33c07c1375334
Fariya Islam <fariyaislam2096@gmail.com>
2021-08-05 12:40:18 +0600

    AboutUs

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,10 +3,15 @@ import './AboutUs.css'
 
 const AboutUs = () => {
     return (
-        <section className="aboutus__parent">
-            <h2>About Us</h2>
+        <div className="bg__svg">
+            <section className="aboutus__parent">
+                <h2>About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut lacus eu risus dignissim tempus in eu ex. Integer a ante sed ex condimentum accumsan. In varius suscipit libero nec vestibulum. Nam condimentum luctus dui non gravida.Nulla aliquet vestibulum egestas.
+                </p>
 
-        </section>
+            </section>
+        </div>
     )
 }
 
