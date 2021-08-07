473cbe0c0f550b07be107ad142d6c24025214a51
Fariya Islam <fariyaislam2096@gmail.com>
2021-08-07 19:14:41 +0600

    COMPLETE

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,9 +4,14 @@ import './ContactUs.css'
 const ContactUs = () => {
     return (
         <section className="contact__parent">
-            <div className="contact__bg">
+            <div className="bg_opacity">
+                <div className="contact__content">
+                    <h1>We want to do something big, so let's do this togather</h1>
+                    <a href="/" >Get a free quote!</a>
+                </div>
             </div>
         </section>
+        
     )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <section className="footer">
+            <div className="col-1">
+                <p> WHY SLACK? <br />Slack vs. Email <br />Channels<br />Engagement Scale </p>
+            </div>
+            <div className="col-2">
+                <p>PRODUCT <br />Features<br />Integrations <br />Enterprise <br />Solutions</p>
+
+            </div>
+            <div className="col-3">
+                <p>PRODUCT <br />Features <br />Integrations <br />Enterprise <br />
+                    Solutions</p>
+            </div>
+            <p>@2021 XYZ agencey, all rights reserved!</p>
+
+        </section>
     )
 }
 
