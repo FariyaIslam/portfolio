e4f50cca00754dd628577cd5ebf3682f5b06beba
Fariya Islam <fariyaislam2096@gmail.com>
2021-08-10 22:06:16 +0600

    Edits

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 const Example = () => {
     let adds = ["xyz", "abc", "ijk"];
+    let humans = [
+        { features: "black", age: "10" },
+        { features: "white", age: "11" },
+        { features: "brown", age: "12" },
+    ]
     return (
         <div>
-            {
-                adds.map(add =>
-                    <h1>{add}</h1>
-                )
-            }
+            {humans.map(human =>
+                <div>
+                    <h1>{human.features}</h1>
+                    <p>{human.age}</p>
+                </div>
+            )}
 
         </div>
     )
